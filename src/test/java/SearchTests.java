5418d55e85ab7f05e61bd8f554b7ce1671bbbe4c
pleso-evg <pleso47@gmail.com>
2025-03-26 00:51:00 +0500

    text selenide

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,9 +8,11 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
+        Configuration.holdBrowserOpen = true;
+
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[id=search]").shouldHave(text("selenide"));
     }
 
     @Test
